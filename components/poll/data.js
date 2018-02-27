15889d500645a9b346c320f33b77d63dfde18787
Edin Rivera <edin.rivera@jet.com>
2018-02-27 10:04:31 -0500

    page layout basis

---
+++ components/poll/data.js
@@ -0,0 +1,16 @@
+const data = {
+	polls: [
+		{
+			name: {
+				firstName: 'Kayne',
+				lastName: 'West',
+			},
+			category: 'Entertainment',
+			photo: '',
+			date: '',
+			fact: '',
+		},
+	],
+}
+
+export default data;
